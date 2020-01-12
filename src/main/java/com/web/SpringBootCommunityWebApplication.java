5c192fc20b9fb3164e0680e39f40867a1541b258
kjkim <apio1130@naver.com>
2020-01-13 00:44:12 +0900

    CommandLineRunner를 Bean으로 등록하여 구동 시 DB에 데이터 등록

--- src/main/java/com/web/SpringBootCommunityWebApplication.java
+++ src/main/java/com/web/SpringBootCommunityWebApplication.java
@@ -1,7 +1,18 @@
 package com.web;
 
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.web.domain.Board;
+import com.web.domain.User;
+import com.web.domain.enums.BoardType;
+import com.web.repository.BoardRepository;
+import com.web.repository.UserRepository;
 
 @SpringBootApplication
 public class SpringBootCommunityWebApplication {
@@ -10,4 +21,27 @@ public class SpringBootCommunityWebApplication {
 		SpringApplication.run(SpringBootCommunityWebApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) throws Exception {
+		return (args) -> {
+			User user = userRepository.save(User.builder()
+					.name("Tester")
+					.password("test")
+					.email("test@test.com")
+					.createdDate(LocalDateTime.now())
+					.updatedDate(LocalDateTime.now())
+					.build());
+			
+			IntStream.range(1, 200).forEach(idx -> boardRepository.save(Board.builder()
+					.title("게시글" + idx)
+					.content("컨텐츠")
+					.boardType(BoardType.free)
+					.createdDate(LocalDateTime.now())
+					.updatedDate(LocalDateTime.now())
+					.user(user)
+					.build()));
+		};
+		
+	}
+	
 }
