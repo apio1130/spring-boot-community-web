4b2ca6f7f5278ad85e3b84a77912b4b60e1db750
kjkim <apio1130@naver.com>
2020-01-13 00:38:49 +0900

    Board와 User Repository 추가 및 테스트 코드 작성

---
+++ src/test/java/com/web/JpaMappingTest.java
@@ -0,0 +1,63 @@
+package com.web;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.web.domain.Board;
+import com.web.domain.User;
+import com.web.domain.enums.BoardType;
+import com.web.repository.BoardRepository;
+import com.web.repository.UserRepository;
+
+@DataJpaTest
+class JpaMappingTest {
+	private final String boardTestTitle = "테스트";
+	private final String email = "test@test.com";
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	BoardRepository boardRepository;
+	
+	@BeforeEach
+	public void init() {
+		System.out.println("init");
+		User user = userRepository.save(User.builder()
+				.name("Tester")
+				.password("test")
+				.email(email)
+				.createdDate(LocalDateTime.now())
+				.updatedDate(LocalDateTime.now())
+				.build());
+		boardRepository.save(Board.builder()
+				.title(boardTestTitle)
+				.content("컨텐츠")
+				.boardType(BoardType.free)
+				.createdDate(LocalDateTime.now())
+				.updatedDate(LocalDateTime.now())
+				.user(user)
+				.build());
+	}
+
+	@Test
+	public void Repository_조회_테스트() {
+		User user = userRepository.findByEmail(email);
+		assertEquals(user.getName(), "Tester");
+		assertEquals(user.getPassword(), "test");
+		assertEquals(user.getEmail(), email);
+		
+		
+		Board board = boardRepository.findByUser(user);
+		assertEquals(board.getTitle(), boardTestTitle);
+		assertEquals(board.getContent(), "컨텐츠");
+		assertEquals(board.getBoardType(), BoardType.free);
+		
+	}
+
+}
